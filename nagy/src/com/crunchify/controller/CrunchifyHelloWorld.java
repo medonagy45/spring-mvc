98c50c678d2296edb8c080ad732b9fe6802dc229
medonagy <medonagy@medo>
2017-03-21 04:42:07 +0200

    this is a clean version of spring mvc working : index and welcome pages not working :  hagaru page

---
+++ nagy/src/com/crunchify/controller/CrunchifyHelloWorld.java
@@ -0,0 +1,31 @@
+
+package com.crunchify.controller;
+ 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+ 
+/*
+ * author: Crunchify.com
+ * 
+ */
+ 
+@Controller
+public class CrunchifyHelloWorld {
+ 
+	@RequestMapping("/welcom")
+	public ModelAndView helloWorld() {
+ 
+		String message = "<br><div style='text-align:center;'>"
+				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
+		return new ModelAndView("welcom", "message", message);
+		
+	}
+	@RequestMapping("/hagaru")
+	public ModelAndView hagerWorld() {
+ 
+		String message = "<br><div style='text-align:center;'>"
+				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
+		return new ModelAndView("hagaru", "messageq", message);}
+	
+}
